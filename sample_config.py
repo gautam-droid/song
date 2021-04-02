9f3892a58f6339e5b6ce3c9ddcb48917fa58106b
gautam-droid <81165310+gautam-droid@users.noreply.github.com>
2021-04-02 21:30:01 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,11 +3,11 @@ HEROKU = True   # NOTE Make it false if you're not deploying on heroku.
 # NOTE these values are for heroku.
 if HEROKU:
     from os import environ
-    API_ID = int(environ["API_ID"])
-    API_HASH = environ["API_HASH"]
+    API_ID = int(environ[1713570])
+    API_HASH = environ[720a2350815be240fbaf1b6aebbb03bd]
     SUDO_CHAT_ID = int(environ["SUDO_CHAT_ID"])
-    OWNER_ID = int(environ["OWNER_ID"])
-    SESSION_STRING = environ["SESSION_STRING"]
+    OWNER_ID = int(environ[1759059779])
+    SESSION_STRING = environ[BQDEP4PVpvrG9fu7MDj4Ht2sGbK62qiLg5QaOfBGdLaSSRA9NDaCRl3NEO1_cpyHyCWubvDBImwZ_lTKBPsUGw62ouy1KPwIt17adHFLAU_5sMT3VtQ9tiFBrLu-g25e-qI2T5_sz2HraeTmBpxx7DyEnV1fuN_sYs1HbB5zD4veoE27osw5I_QJOj0YfIgeV0bTkGZoFe9P37IVTOojlFs_UmFm_zf7wkED_ZtfdeRVIOnp58_oHKOUl3_qyEzrQKY0KrxaQrSoduzUf9tXr9wLDphctFihh6T2uYtmLeODctLxwLf_jEx4QOwG56y0LKrQmSTE6USOhX1hUXg13OMHaNkfQwA]
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
