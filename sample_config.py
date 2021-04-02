7fc50deeaa0e9cacd24f41b4791a64d8f4a61178
gautam-droid <81165310+gautam-droid@users.noreply.github.com>
2021-04-02 21:35:14 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,7 +5,7 @@ if HEROKU:
     from os import environ
     API_ID = int(environ[1713570])
     API_HASH = environ[720a2350815be240fbaf1b6aebbb03bd]
-    SUDO_CHAT_ID = int(environ["SUDO_CHAT_ID"])
+    SUDO_CHAT_ID = int(environ[1797645753])
     OWNER_ID = int(environ[1759059779])
     SESSION_STRING = environ[BQDEP4PVpvrG9fu7MDj4Ht2sGbK62qiLg5QaOfBGdLaSSRA9NDaCRl3NEO1_cpyHyCWubvDBImwZ_lTKBPsUGw62ouy1KPwIt17adHFLAU_5sMT3VtQ9tiFBrLu-g25e-qI2T5_sz2HraeTmBpxx7DyEnV1fuN_sYs1HbB5zD4veoE27osw5I_QJOj0YfIgeV0bTkGZoFe9P37IVTOojlFs_UmFm_zf7wkED_ZtfdeRVIOnp58_oHKOUl3_qyEzrQKY0KrxaQrSoduzUf9tXr9wLDphctFihh6T2uYtmLeODctLxwLf_jEx4QOwG56y0LKrQmSTE6USOhX1hUXg13OMHaNkfQwA]
 
